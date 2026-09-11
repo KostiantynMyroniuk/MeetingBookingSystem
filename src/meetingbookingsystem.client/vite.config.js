0fd2d3e23c29d150a552eab41dade08c83c1d358
KostyaMuroniuk <muronandrey2@gmail.com>
2026-09-11 23:28:35 +0300

    fix: vite config env fix

--- src/meetingbookingsystem.client/vite.config.js
+++ src/meetingbookingsystem.client/vite.config.js
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import plugin from '@vitejs/plugin-react';
-const API_URL = import.meta.env.API_URL;
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        proxy: {
-            '/api': {
-                target: API_URL,
-                changeOrigin: true,
-                secure: false,
-            },
-            '/hubs': {
-                target: API_URL,
-                changeOrigin: true,
-                secure: false,
-                ws: true
+
+export default defineConfig(({ mode }) => {
+
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [plugin()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                },
+                '/hubs': {
+                    target: env.API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                    ws: true
+                }
             }
         }
-    }
-})
+    };
+});
