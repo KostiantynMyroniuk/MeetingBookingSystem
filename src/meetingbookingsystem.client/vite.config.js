c79a8e14b9184786ac0a6e344a214883f64e959e
KostyaMuroniuk <muronandrey2@gmail.com>
2026-09-11 23:40:58 +0300

    fix: revert changes

--- src/meetingbookingsystem.client/vite.config.js
+++ src/meetingbookingsystem.client/vite.config.js
@@ -1,27 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
 
-export default defineConfig(({ mode }) => {
-
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        plugins: [plugin()],
-        server: {
-            proxy: {
-                '/api': {
-                    target: env.API_URL,
-                    changeOrigin: true,
-                    secure: false,
-                },
-                '/hubs': {
-                    target: env.API_URL,
-                    changeOrigin: true,
-                    secure: false,
-                    ws: true
-                }
-            }
-        }
-    };
+export default defineConfig({
+    plugins: [plugin()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'localhost:5241',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/hubs': {
+                target: 'localhost:5241',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            },
+        },
+    },
 });
